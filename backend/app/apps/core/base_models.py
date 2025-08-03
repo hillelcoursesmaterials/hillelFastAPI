6e226619d2fd612c801520281e69bfa1abecb1eb
hillelcoursesmaterials <hillelcourses24@gmail.com>
2025-08-03 13:33:00 +0300

    8-2-save-user-in-db (#10)
    * add password to user table
    
    * save user
    
    ---------
    
    Co-authored-by: hillelcourses <hillelcourses@ukr.net>

--- backend/app/apps/core/base_models.py
+++ backend/app/apps/core/base_models.py
@@ -1,9 +1,16 @@
 from datetime import datetime
 
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
-from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.ext.asyncio import AsyncAttrs, create_async_engine, async_sessionmaker
 from sqlalchemy.sql import func
 
+from settings import settings
+
+engine = create_async_engine(
+    settings.DATABASE_ASYNC_URL,
+    echo=settings.DEBUG,
+)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
