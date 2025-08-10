ccf11401b246f9528c008bc189a23e34d7aeac6d
hillelcoursesmaterials <hillelcourses24@gmail.com>
2025-08-10 16:09:16 +0300

    cannot duplicate user (#11)
    Co-authored-by: hillelcourses <hillelcourses@ukr.net>

--- backend/app/apps/core/base_crud.py
+++ backend/app/apps/core/base_crud.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
 from apps.core.base_models import Base
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import InstrumentedAttribute
+from sqlalchemy import select
 from fastapi import HTTPException, status
-from typing import Optional
+from typing import Optional, Any
 class BaseCRUDManager(ABC):
     model: type[Base] = None
 
@@ -10,7 +12,7 @@ class BaseCRUDManager(ABC):
     def __init__(self):
         pass
 
-    async def create_instance(self, *, session: AsyncSession, **kwargs) -> Optional[Base]:
+    async def create(self, *, session: AsyncSession, **kwargs) -> Optional[Base]:
         instance = self.model(**kwargs)
         session.add(instance)
         try:
@@ -22,3 +24,8 @@ class BaseCRUDManager(ABC):
                 detail=f"Error has occurred while creating {self.model} instance with {kwargs}, {e=}",
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
+
+    async def get(self, *, session: AsyncSession, field_value: Any, field: InstrumentedAttribute) -> Optional[Base]:
+        query = select(self.model).filter(field==field_value)
+        result = await session.execute(query)
+        return result.scalar_one_or_none()
--- backend/app/apps/users/crud.py
+++ backend/app/apps/users/crud.py
@@ -1,4 +1,5 @@
 from apps.core.base_crud import BaseCRUDManager
+from fastapi import HTTPException, status
 from .models import User
 from .schemas import RegisterUserSchema
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -10,8 +11,15 @@ class UserCRUDManager(BaseCRUDManager):
         self.model = User
 
     async def create_user(self, new_user: RegisterUserSchema, session: AsyncSession) -> User:
+        maybe_user = await self.get(session=session, field=self.model.email, field_value=new_user.email)
+        if maybe_user:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"User with email {new_user.email} already exists",
+            )
+
         hashed_password = await PasswordEncrypt.get_password_hash(new_user.password)
-        user = await self.create_instance(
+        user = await self.create(
             session=session,
             email=new_user.email,
             hashed_password=hashed_password,
