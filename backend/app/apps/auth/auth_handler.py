41bfca53084ad4332bf12c0adee0706bb1bd4919
hillelcourses <hillelcourses@ukr.net>
2025-09-14 18:00:08 +0300

    create tokens

--- backend/app/apps/auth/auth_handler.py
+++ backend/app/apps/auth/auth_handler.py
@@ -1,3 +1,7 @@
+import datetime as dt
+from uuid import uuid4
+
+import jwt
 from apps.users.crud import User, user_manager
 from fastapi import HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
@@ -35,9 +39,42 @@ class AuthHandler:
                 detail="Incorrect password", status_code=status.HTTP_403_FORBIDDEN
             )
 
-        return LoginResponseSchema(
-            access_token="access_token", refresh_token="refresh_token", expired_at=123
+        tokens_response = await self.generate_tokens(user)
+        return tokens_response
+
+    async def generate_tokens(self, user: User) -> LoginResponseSchema:
+        access_token_payload = {
+            "sub": str(user.id),
+            "email": user.email,
+        }
+        access_token = await self.generate_token(
+            access_token_payload, self.access_token_lifetime
         )
+
+        refresh_token_payload = {
+            "sub": str(user.id),
+            "email": user.email,
+            "key": uuid4().hex,
+        }
+        refresh_token = await self.generate_token(
+            refresh_token_payload, self.refresh_token_lifetime
+        )
+
+        return LoginResponseSchema(
+            access_token=access_token,
+            refresh_token=refresh_token,
+            expired_at=self.access_token_lifetime * 60,
+        )
+
+    async def generate_token(self, payload: dict, expire_minutes: int) -> str:
+        now = dt.datetime.now()
+        token_expires_at = dt.timedelta(minutes=expire_minutes)
+        time_payload = {"exp": now + token_expires_at, "iat": now}
+        payload.update(time_payload)
+        print(payload)
+        token_ = jwt.encode(payload, self.jwt_secret, self.jwt_algorithm)
+        print(token_)
+        return token_
 
 
 auth_handler = AuthHandler()
