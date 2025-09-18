68aecfff86497816b61b677c57f7095b56a03161
hillelcoursesmaterials <hillelcourses24@gmail.com>
2025-09-19 00:16:04 +0300

    permissions added (#24)
    Co-authored-by: hillelcourses <hillelcourses@ukr.net>

--- backend/app/apps/auth/dependencies.py
+++ backend/app/apps/auth/dependencies.py
@@ -1,3 +1,6 @@
+from enum import StrEnum
+from typing import Callable
+
 from apps.core.dependencies import get_async_session
 from apps.users.crud import User, user_manager
 from fastapi import Depends, HTTPException, status
@@ -34,3 +37,23 @@ async def get_admin_user(user: User = Depends(get_current_user)) -> User:
     raise HTTPException(
         status_code=status.HTTP_403_FORBIDDEN, detail="Admin user is required"
     )
+
+
+def require_permissions(required_permissions: list[StrEnum]) -> Callable:
+    async def dependency(user: User = Depends(get_current_user)) -> User:
+        if user.is_admin:
+            return user
+        user_permissions = set(user.permissions)
+        required_permissions_set: set[str] = {
+            perm.value for perm in required_permissions
+        }
+
+        if required_permissions_set.issubset(user_permissions):
+            return user
+
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"Permissions {', '.join(required_permissions_set)} required",
+        )
+
+    return dependency
---
+++ backend/app/apps/users/constants.py
@@ -0,0 +1,6 @@
+from enum import StrEnum
+
+
+class UserPermissionsEnum(StrEnum):
+    CAN_SEE_USERS = "CAN_SEE_USERS"
+    CAN_SELF_DELETE = "CAN_SELF_DELETE"
