8ec6185019a96f401db2af0160991ab74f0932ff
hillelcourses <hillelcourses@ukr.net>
2025-09-19 00:15:17 +0300

    permissions added

--- backend/app/apps/users/models.py
+++ backend/app/apps/users/models.py
@@ -1,5 +1,9 @@
 from apps.core.base_models import Base
+from sqlalchemy import String, text
+from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.orm import Mapped, mapped_column
+
+from .constants import UserPermissionsEnum
 
 
 class User(Base):
@@ -7,3 +11,9 @@ class User(Base):
     email: Mapped[str] = mapped_column(unique=True)
     hashed_password: Mapped[str]
     is_admin: Mapped[bool] = mapped_column(default=False, nullable=True)
+    permissions: Mapped[list[str]] = mapped_column(
+        ARRAY(String),
+        default=lambda: [UserPermissionsEnum.CAN_SELF_DELETE],
+        nullable=False,
+        server_default=text("'{CAN_SELF_DELETE}'::text[]"),
+    )
--- backend/app/apps/users/router.py
+++ backend/app/apps/users/router.py
@@ -1,8 +1,9 @@
-from apps.auth.dependencies import get_admin_user, get_current_user
+from apps.auth.dependencies import get_current_user, require_permissions
 from apps.core.dependencies import get_async_session
 from fastapi import APIRouter, Depends, HTTPException, Path, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from .constants import UserPermissionsEnum
 from .crud import User, user_manager
 from .schemas import RegisteredUserSchema, RegisterUserSchema
 
@@ -23,7 +24,10 @@ async def get_my_info(user: User = Depends(get_current_user)) -> RegisteredUserS
     return RegisteredUserSchema.from_orm(user)
 
 
-@router_users.get("/{id}", dependencies=[Depends(get_admin_user)])
+@router_users.get(
+    "/{id}",
+    dependencies=[Depends(require_permissions([UserPermissionsEnum.CAN_SEE_USERS]))],
+)
 async def get_user(
     user_id: int = Path(..., description="The id of the user", ge=1, alias="id"),
     session: AsyncSession = Depends(get_async_session),
