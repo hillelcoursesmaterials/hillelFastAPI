d95914268cd80b4984918cc15c329eb39d33f5af
hillelcourses <hillelcourses@ukr.net>
2025-10-15 22:01:17 +0300

    14-2 fix precommit

--- backend/app/migrations/env.py
+++ backend/app/migrations/env.py
@@ -4,6 +4,8 @@ from alembic import context
 from apps.core.base_models import Base
 from settings import settings
 from sqlalchemy import engine_from_config, pool
+from apps.users.models import User
+from apps.products.models import Category
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
