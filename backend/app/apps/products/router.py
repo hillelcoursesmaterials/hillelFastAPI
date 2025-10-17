391abaeeaeb6a98e7a8d73041455bb9f968f15cc
hillelcourses <hillelcourses@ukr.net>
2025-10-17 09:38:51 +0300

    14-3 get categories

--- backend/app/apps/products/router.py
+++ backend/app/apps/products/router.py
@@ -1,9 +1,16 @@
+from typing import Annotated
+
 from apps.auth.dependencies import require_permissions
 from apps.core.dependencies import get_async_session
+from apps.core.schemas import SearchParamsSchema
 from apps.products.crud import Category, category_manager
-from apps.products.schemas import NewCategory, SavedCategorySchema
+from apps.products.schemas import (
+    NewCategory,
+    PaginatorSavedCategoryResponseSchema,
+    SavedCategorySchema,
+)
 from apps.users.constants import UserPermissionsEnum
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Path, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 router_categories = APIRouter()
@@ -33,3 +40,33 @@ async def create_category(
         **new_category.dict(), session=session
     )
     return saved_category
+
+
+@router_categories.get("/{id}")
+async def get_category_by_id(
+    category_id: int = Path(..., description="The id of the item", ge=1, alias="id"),
+    session: AsyncSession = Depends(get_async_session),
+) -> SavedCategorySchema:
+    saved_category = await category_manager.get(
+        field=Category.id, field_value=category_id, session=session
+    )
+    if not saved_category:
+        raise HTTPException(
+            detail=f"Category with id '{category_id}' not found",
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
+    return saved_category
+
+
+@router_categories.get("/")
+async def get_categories(
+    params: Annotated[SearchParamsSchema, Depends()],
+    session: AsyncSession = Depends(get_async_session),
+) -> PaginatorSavedCategoryResponseSchema:
+    result = await category_manager.get_items_paginated(
+        session=session,
+        search_fields=[Category.name],
+        targeted_schema=SavedCategorySchema,
+        params=params,
+    )
+    return result
