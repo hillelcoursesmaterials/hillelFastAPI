1b933c1ac88f787afb5aea7183e2d25bdc132651
hillelcourses <hillelcourses@ukr.net>
2025-10-19 22:26:52 +0300

    14-5 create products table

--- backend/app/apps/products/models.py
+++ backend/app/apps/products/models.py
@@ -1,19 +1,32 @@
-import datetime as dt
-
-from apps.core.base_models import Base
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy.sql import func
+from apps.core.base_models import Base, UpdatedAtMixin, UUIDMixin
+from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
-class Category(Base):
+class Category(UpdatedAtMixin, Base):
     __tablename__ = "categories"
 
-    name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(70), unique=True, nullable=False)
     version: Mapped[int] = mapped_column(Integer, nullable=False, default=1)
-    updated_at: Mapped[dt.datetime] = mapped_column(
-        default=func.now(), onupdate=func.now()
-    )
+
+    products = relationship("Product", back_populates="category")
 
     def __str__(self) -> str:
         return f"<Category {self.name} - #{self.id}>"
+
+
+class Product(UpdatedAtMixin, UUIDMixin, Base):
+    title: Mapped[str] = mapped_column(String(200), nullable=False)
+    description: Mapped[str] = mapped_column(String(2048), default="")
+    price: Mapped[float] = mapped_column(nullable=False)
+    main_image: Mapped[str] = mapped_column(nullable=False)
+    images: Mapped[list[str]] = mapped_column(ARRAY(String), default=list)
+    category_id: Mapped[int] = mapped_column(
+        ForeignKey("categories.id", ondelete="RESTRICT"), nullable=False
+    )
+
+    category = relationship("Category", back_populates="products")
+
+    def __str__(self) -> str:
+        return f"<Product {self.title} - #{self.id}, current price: {self.price}>"
--- backend/app/migrations/env.py
+++ backend/app/migrations/env.py
@@ -3,7 +3,7 @@ from logging.config import fileConfig
 from alembic import context
 from apps.core.base_models import Base
 from apps.users.models import User
-from apps.products.models import Category
+from apps.products.models import Category, Product
 from settings import settings
 from sqlalchemy import engine_from_config, pool
 
